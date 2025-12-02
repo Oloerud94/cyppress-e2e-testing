38b50513f24f4e372a5bf41c67cd2bd2b7c85119
oloerud94 <oloerud94@gmail.com>
2025-12-02 08:14:38 +0200

    mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'http://localhost:3000/',
     supportFile: false,
     // fixturesFolder: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     env: {
       users: {
